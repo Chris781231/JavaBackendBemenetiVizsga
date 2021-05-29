b0977c008547c1dc628c2a1ae1d18c65a321409b
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-05-29 14:04:09 +0200

    Add `longestword` solution.

---
+++ src/main/java/longestword/LongestWord.java
@@ -0,0 +1,43 @@
+package longestword;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class LongestWord {
+
+    private String longestWord = "";
+    private int maxLength = 0;
+
+    private static String WORD_SEPARATOR = " ";
+
+    public String findLongestWord(String fileName) {
+        if (isEmpty(fileName)) throw new IllegalArgumentException("invalid filename");
+
+        Path path = Path.of(fileName);
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                processLine(line);
+            }
+        } catch (IOException ioe) {
+            return "";
+        }
+        return longestWord;
+    }
+
+    private void processLine(String line) {
+        String[] parts = line.split(WORD_SEPARATOR);
+        for (String item : parts) {
+            if (item.length() > maxLength) {
+                maxLength = item.length();
+                longestWord = item;
+            }
+        }
+    }
+
+    private boolean isEmpty(String fileName) {
+        return fileName == null || fileName.isBlank();
+    }
+}
