1dece39e05abb6001e1174347ba297b26cb9d2fd
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-05-29 11:29:19 +0200

    Init commit.

---
+++ src/test/java/birdwatcher/BirdWatcherTest.java
@@ -0,0 +1,61 @@
+//package birdwatcher;
+//
+//import org.flywaydb.core.Flyway;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mariadb.jdbc.MariaDbDataSource;
+//
+//import java.sql.SQLException;
+//import java.util.List;
+//
+//public class BirdWatcherTest {
+//
+//    private static final List<String> EXPECTED_SPECIES = List.of("american robin", "downy woodpecker", "house sparrow", "northern cardinal");
+//
+//    private BirdWatcher birdWatcher;
+//
+//    @BeforeEach
+//    void init() throws SQLException {
+//        MariaDbDataSource dataSource;
+//        dataSource = new MariaDbDataSource();
+//        dataSource.setUrl("jdbc:mariadb://localhost:3306/employees?useUnicode=true");
+//        dataSource.setUser("employees");
+//        dataSource.setPassword("employees");
+//
+//        Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+//
+//        flyway.clean();
+//        flyway.migrate();
+//
+//        birdWatcher = new BirdWatcher(dataSource);
+//
+//    }
+//
+//
+//    @Test
+//    void test_getUniqueBirds_allBirdsOnce_anyOrder() {
+//        List<String> actualSpecies = birdWatcher.getUniqueBirds();
+//
+//        assertEquals(EXPECTED_SPECIES.size(), actualSpecies.size());
+//        for (String bird : EXPECTED_SPECIES) {
+//            assertTrue(actualSpecies.contains(bird));
+//        }
+//    }
+//
+//    @Test
+//    void test_getUniqueBirds_allBirdsOnce_alphabeticOrder() {
+//        List<String> actualSpecies = birdWatcher.getUniqueBirds();
+//
+//        assertEquals(EXPECTED_SPECIES, actualSpecies);
+//    }
+//
+//    @Test
+//    void test_getUniqueBirds_oneDuplicate_anyOrder() {
+//        List<String> actualSpecies = birdWatcher.getUniqueBirds();
+//
+//        assertEquals(EXPECTED_SPECIES.size(), actualSpecies.size());
+//        for (String bird : EXPECTED_SPECIES) {
+//            assertTrue(actualSpecies.contains(bird));
+//        }
+//    }
+//}
